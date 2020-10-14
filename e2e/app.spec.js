2b791a85508d847bce0e59fa3ddb9a4a22b1664f
chandankkrr <chandankkrr@gmail.com>
2020-10-14 16:18:15 +0700

    Initial commit, running e2e via github actions

---
+++ e2e/app.spec.js
@@ -0,0 +1,13 @@
+const { reloadApp } = require('detox-expo-helpers')
+
+describe('order', () => {
+  beforeEach(async () => {
+    await reloadApp()
+    await waitFor(element(by.id("tab-home")))
+      .toBeVisible()
+      .withTimeout(5000)
+  })
+  it('App start', async () => {
+    await expect(element(by.id("tab-one1"))).toBeVisible()
+  })
+})
