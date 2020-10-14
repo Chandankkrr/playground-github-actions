0c547977ec7f96df0b0e5bb0753f95f9e48c7c11
Chandan Rauniyar <chandankkrr@gmail.com>
2020-10-14 20:42:38 +0700

    Update init.js

--- e2e/init.js
+++ e2e/init.js
@@ -1,6 +1,7 @@
 const detox = require('detox')
 const adapter = require('detox/runners/jest/adapter')
 const specReporter = require('detox/runners/jest/specReporter')
+const config = require('../package.json').detox;
 
 // Set the default timeout
 jest.setTimeout(120000)
@@ -12,7 +13,7 @@ jasmine.getEnv().addReporter(adapter)
 jasmine.getEnv().addReporter(specReporter)
 
 beforeAll(async () => {
-  await detox.init()
+  await detox.init(config)
 }, 300000)
 
 beforeEach(async () => {
