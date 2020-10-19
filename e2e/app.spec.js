b65e2a2113dc71566d5e25a989d7d1e02d1a21ae
Chandan Rauniyar <chandankkrr@gmail.com>
2020-10-19 09:59:55 +0700

    Update app.spec.js

--- e2e/app.spec.js
+++ e2e/app.spec.js
@@ -3,6 +3,9 @@ const { reloadApp } = require('detox-expo-helpers')
 describe('order', () => {
   beforeEach(async () => {
     await reloadApp()
+    await device.setURLBlacklist([
+      '.*10\.4\.12\.38.*'
+    ]);
     await waitFor(element(by.id("tab-home")))
       .toBeVisible()
       .withTimeout(5000)
